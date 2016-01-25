69c1e5507f008750ef38f527f5e007604af28313
connor harrison clark <clarkcon@beta.cse.ohio-state.edu>
2016-01-25 17:38:36 -0500

    added server side code from project 1

--- lab2/ftps.c
+++ lab2/ftps.c
@@ -29,33 +29,64 @@ long get_port(int argc, char *argv[]){
 	}
 }
 
-int main(int argc, char *argv[]){
-	unsigned int server_s;	//server socked descriptor
-	struct sockaddr_in server_addr;	// server internet address
-	unsigned int connect_s;	// connection socket descriptor
-	struct sockaddr_in client_addr;	// client internet address
-	struct in_addr client_ip; // client ip address
-	int addr_len;	// internet address length
-	int port;	// local port-number
-	if(port = get_port(argc, argv) < 0 ){
-		exit(1);
-	}
-	server_s = socket(AF_INET, SOCK_STREAM, 0);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if(bind(server_s, (struct sockaddr *)&server_addr, sizeof(server_addr)) <0){
-		printf("Error could not bind socket\n");
-		exit(1);
-	}
-	listen(server_s, 1);
-	addr_len = sizeof(client_addr);
-	//blocking accept connection call
-	connect_s = accept(server_s, (struct sockaddr *) &client_addr, &addr_len);
-	if(connect_s <0){
-		printf("Error accepting connection\n");
-		exit(1);
-	}
-	memcpy(&client_ip, &client_addr.sin_addr.s_addr, 4);
-	printf("Accept completed\nIP of client: %s	port: %d \n", inet_ntoa(client_ip), ntohs(client_addr.sin_port));
+/* server program called with no argument */
+main(void)
+{
+  int sock;                     /* initial socket descriptor */
+  int msgsock;                  /* accepted socket descriptor,
+                                 * each client connection has a
+                                 * unique socket descriptor*/
+  int rval=1;                   /* returned value from a read */  
+  struct sockaddr_in sin_addr; /* structure for socket name setup */
+  char buf[1024];               /* buffer for holding read data */
+  char buf2[1024] = "Hello back in TCP from server"; 
+
+  printf("TCP server waiting for remote connection from clients ...\n");
+ 
+  /*initialize socket connection in unix domain*/
+  if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+  perror("error openting datagram socket");
+  exit(1);
+  }
+  
+  /* construct name of socket to send to */
+  sin_addr.sin_family = AF_INET;
+  sin_addr.sin_addr.s_addr = INADDR_ANY;
+  sin_addr.sin_port = htons(atoi(port));
+
+  /* bind socket name to socket */
+  if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
+    perror("error binding stream socket");
+    exit(1);
+  }
+  
+  /* listen for socket connection and set max opened socket connetions to 5 */
+  listen(sock, 5);
+  
+  /* accept a (1) connection in socket msgsocket */ 
+  if((msgsock = accept(sock, (struct sockaddr *)NULL, (int *)NULL)) == -1) { 
+    perror("error connecting stream socket");
+    exit(1);
+  } 
+  
+  /* put all zeros in buffer (clear) */
+  bzero(buf,1024);
+
+  /* read from msgsock and place in buf */
+  if(read(msgsock, buf, 1024) < 0) {
+    perror("error reading on stream socket");
+    exit(1);
+  } 
+  printf("Server receives: %s\n", buf);
+ 
+  /* write message back to client */
+  if(write(msgsock, buf2, 1024) < 0) {
+    perror("error writing on stream socket");
+    exit(1);
+  }
+  printf("Server sends:    %s\n", buf2);
+ 
+  /* close all connections and remove socket file */
+  close(msgsock);
+  close(sock);
 }
