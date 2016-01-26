fa3a7ae8dedf5a119226b1abd49f4735e6e1cab8
curtisholton1 <curtisholton1@outlook.com>
2016-01-26 18:58:40 -0500

    changed spaces to tabs

--- lab2/ftps.c
+++ lab2/ftps.c
@@ -32,65 +32,58 @@ long get_port(int argc, char *argv[]){
 /* server program called with no argument */
 main(int argc, char* argv[])
 {
-  int sock;                     /* initial socket descriptor */
-  int msgsock;                  /* accepted socket descriptor,
+ 	int sock;                     /* initial socket descriptor */
+	int msgsock;                  /* accepted socket descriptor,
                                  * each client connection has a
                                  * unique socket descriptor*/
-  int rval=1;                   /* returned value from a read */  
-  struct sockaddr_in sin_addr; /* structure for socket name setup */
-  char buf[1024];               /* buffer for holding read data */
-  char buf2[1024] = "Hello back in TCP from server"; 
-
-  printf("TCP server waiting for remote connection from clients ...\n");
- 
-  /*initialize socket connection in unix domain*/
-  if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-  perror("error openting datagram socket");
-  exit(1);
-  }
-  long port = get_port(argc,argv);
-  if(port<0)
+	int rval=1;                   /* returned value from a read */  
+	struct sockaddr_in sin_addr; /* structure for socket name setup */
+	char buf[1024];               /* buffer for holding read data */
+	char buf2[1024] = "Hello back in TCP from server"; 
+	printf("TCP server waiting for remote connection from clients ...\n");
+	/*initialize socket connection in unix domain*/
+	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	perror("error openting datagram socket");
+	exit(1);
+	}
+	long port = get_port(argc,argv);
+	if(port<0)
 	{
 		exit(1);
 	}  	
-  /* construct name of socket to send to */
-  sin_addr.sin_family = AF_INET;
-  sin_addr.sin_addr.s_addr = INADDR_ANY;
-  sin_addr.sin_port = htonl(port);
-
-  /* bind socket name to socket */
-  if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
-    perror("error binding stream socket");
-    exit(1);
-  }
-  
-  /* listen for socket connection and set max opened socket connetions to 5 */
-  listen(sock, 5);
-  
-  /* accept a (1) connection in socket msgsocket */ 
-  if((msgsock = accept(sock, (struct sockaddr *)NULL, (int *)NULL)) == -1) { 
-    perror("error connecting stream socket");
-    exit(1);
-  } 
-  
-  /* put all zeros in buffer (clear) */
-  bzero(buf,1024);
-
-  /* read from msgsock and place in buf */
-  if(read(msgsock, buf, 1024) < 0) {
-    perror("error reading on stream socket");
-    exit(1);
-  } 
-  printf("Server receives: %s\n", buf);
- 
-  /* write message back to client */
-  if(write(msgsock, buf2, 1024) < 0) {
-    perror("error writing on stream socket");
-    exit(1);
-  }
-  printf("Server sends:    %s\n", buf2);
- 
-  /* close all connections and remove socket file */
-  close(msgsock);
-  close(sock);
+	printf("port:   %u\n", port);
+	/* construct name of socket to send to */
+	sin_addr.sin_family = AF_INET;
+	sin_addr.sin_addr.s_addr = INADDR_ANY;
+	sin_addr.sin_port = port;
+	printf("port:   %u\n", sin_addr.sin_port);
+	/* bind socket name to socket */
+	if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) {
+		perror("error binding stream socket");
+ 	   exit(1);
+	}
+	/* listen for socket connection and set max opened socket connetions to 5 */
+	listen(sock, 5);
+	/* accept a (1) connection in socket msgsocket */ 
+	if((msgsock = accept(sock, (struct sockaddr *)NULL, (int *)NULL)) == -1) { 
+ 	   perror("error connecting stream socket");
+ 	   exit(1);
+	}
+	/* put all zeros in buffer (clear) */
+	bzero(buf,1024);
+	/* read from msgsock and place in buf */
+	if(read(msgsock, buf, 1024) < 0) {
+		perror("error reading on stream socket");
+		exit(1);
+	} 
+	printf("Server receives: %s\n", buf);
+	/* write message back to client */
+	if(write(msgsock, buf2, 1024) < 0) {
+		perror("error writing on stream socket");
+		exit(1);
+	}
+	printf("Server sends:    %s\n", buf2);
+	/* close all connections and remove socket file */
+	close(msgsock);
+	close(sock);
 }
