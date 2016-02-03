cd79d5b5267a1f5e5a9f9d99abe2a70c803eb1c0
curtisholton1 <curtisholton1@outlook.com>
2016-02-03 16:57:11 -0500

    ftps

--- lab3/ftps.c
+++ lab3/ftps.c
@@ -35,12 +35,27 @@ int get_port(int argc, char *argv[]){
 	}
 }
 
+/*
+should send a packet to tcpd, then wait for tcpd to send data back
+*/
+ssize_t RECV(int sockfd, void *buf, size_t len, int flags){
+	struct sockaddr_in src_addr;
+	int addr_len=sizeof(struct sockaddr_in);
+	//send message to tcpd
+	sendto(sockfd, buf, len, flags, NULL, 0);
+	return recvfrom(sockfd, buf, len, flags, (struct sockaddr *)&src_addr , &addr_len);
+}
+
+int SOCKET(int domain, int type, int protocol){
+	return socket(AF_INET, SOCK_DGRAM, 0);
+}
+
 /* server program called with no argument */
 main(int argc, char* argv[])
 {
  	int sock;		/* initial socket descriptor */
-	int msgsock;		/* accepted socket descriptor, each client connection has a unique socket descriptor*/
 	struct sockaddr_in sin_addr;		/* structure for socket name setup */
+	int addr_len;
 	char buf[1000];		/* buffer for holding read data */
 	int size_in_network[4];
 	long long file_size;
@@ -50,43 +65,39 @@ main(int argc, char* argv[])
 	int total_recv = 0;
 	printf("TCP server waiting for remote connection from clients ...\n");
 	/*initialize socket connection in unix domain*/
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if((sock = SOCKET(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
 		perror("error opening datagram socket");
 		exit(1);
 	}
-	/* construct name of socket to send to */
-	sin_addr.sin_family = AF_INET;
-	sin_addr.sin_addr.s_addr = INADDR_ANY;
-	if((sin_addr.sin_port = htons(get_port(argc, argv))) < 0)
-	{
-		exit(1);
-	}
-	/* bind socket name to socket */
-	if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(struct sockaddr_in)) < 0) 
-	{
-		perror("error binding stream socket");
-		exit(1);
-	}
-	/* listen for socket connection and set max opened socket connetions to 5 */
-	listen(sock, 5);
-	/* accept a (1) connection in socket msgsocket */ 
-	if((msgsock = accept(sock, (struct sockaddr *)NULL, (int *)NULL)) == -1) 
-	{ 
- 	   perror("error connecting stream socket");
- 	   exit(1);
-	}
+	
+	
+	 /* create name with parameters and bind name to socket */
+    sin_addr.sin_family = AF_INET;
+    sin_addr.sin_port = get_port(argc, argv);
+    sin_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(sock, (struct sockaddr *)&sin_addr, sizeof(sin_addr)) < 0) {
+	perror("getting socket name");
+	exit(2);
+    }
+    addr_len=sizeof(struct sockaddr_in);
+    /* Find assigned port value and print it for client to use */
+    if(getsockname(sock, (struct sockaddr *)&sin_addr, &addr_len) < 0){
+	perror("getting sock name");
+	exit(3);
+    }
+    printf("Server waiting on port # %d\n", sin_addr.sin_port);
 	/* put all zeros in buffer (clear) */
 	bzero(buf,1000);
 	/* read from msgsock and place in buf */
-	if(recv(msgsock,size_in_network,4,MSG_WAITALL)<0)
+	if(RECV(sock,size_in_network,4,MSG_WAITALL)<0)
 	{
-		perror("error reading on stream socket");
+		perror("error reading on datagram socket");
 		exit(1);
 	}
 	file_size = ntohl(*size_in_network);
 	printf("Server receives filesize: %d\n",file_size);
-	if(recv(msgsock,name,20,MSG_WAITALL)<0)
+	if(RECV(sock,name,20,MSG_WAITALL)<0)
 	{
 		perror("error reading on stream socket");
 		exit(1);
@@ -117,7 +128,7 @@ main(int argc, char* argv[])
 	/* read from msgsock and place in buf */
 	while(total_recv < file_size)
 	{
-		received =recv(msgsock, buf, 1000,0);
+		received =RECV(sock, buf, 1000,0);
 		total_recv += received;
 		if((fwrite(buf,1,received,out))!= received){
 			perror("ERROR writing to file");
@@ -125,7 +136,6 @@ main(int argc, char* argv[])
 		}
 	}
 	/* close all connections and remove socket file */
-	close(msgsock);
 	close(sock);
 	fclose(out);
 }
