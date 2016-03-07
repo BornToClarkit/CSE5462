9ca16613e977d49a739f023737310c587b7d2cd6
curtisholton1 <curtisholton1@outlook.com>
2016-03-07 18:06:09 -0500

    added final project folder

---
+++ finalProject/ftpc.c
@@ -0,0 +1,152 @@
+/* client.c using UDP */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<errno.h>
+#include<limits.h>
+#include <strings.h>
+#include <string.h>
+#include <linux/tcp.h>
+#include "CapitalFunctions.h"
+//client
+
+/*  Connor Clark
+	Curtis Holton
+*/
+
+
+long check_args(int argc, char *argv[]){
+	if(argc ==  4 && argv[1] != NULL && argv[2] != NULL && argv[3] != NULL)
+	{
+		if(strlen(argv[3]) >= 19)
+		{
+			printf("File name is too large\n");
+			return -1;
+		}
+		return 0;
+	}
+	else 
+	{
+		printf("Invalid number of arguments or an argument was null\ncommand should look like this:\n");
+		printf("ftpc <remote-host> <port-number> <local-file-to-transfer> \n");
+		return -1;
+	}
+}
+
+//checks arg, returns port# if valid, if not returns -1;
+int get_port(char *argv[]){
+	if(argv[2] != NULL)
+	{
+		char * endptr;
+		int val;
+		errno = 0;
+		val = strtol(argv[2], &endptr, 10);
+		if(errno !=0 && (val == LONG_MAX || val == LONG_MIN) || (errno !=0 && val==0) || endptr == argv[1] || val < 0)	
+		{
+			printf("Error converting port-number.\n");
+			val = -1;
+		}
+		return val;
+	}
+	else 
+	{
+		return -1;
+	}
+}
+
+//used to find the size of the file
+int get_file_size(FILE *fp){
+	fseek(fp, 0, SEEK_END);
+	int size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	return size;
+}
+
+/* client program called with host name and port where server is run */
+main(int argc, char *argv[])
+{
+	int sock;                     /* initial socket descriptor */
+	int rval;                    /* returned value from a read */
+	struct sockaddr_in sin_addr; /* structure for socket name 
+                                 * setup */
+  struct sockaddr_in to_send;
+  struct Packet packet;
+	char buf[1060];
+	struct hostent *hp;
+	int pie;
+	int *file_size = NULL;
+	file_size = malloc(sizeof(int));
+	char file_name[20];
+	if(check_args(argc, argv)!=0)
+	{
+		exit(1);
+	}
+	strcpy(file_name, argv[3]);
+	FILE *ifp = fopen(file_name, "rb");
+	if(!ifp)
+	{	
+		printf("test\n");
+		printf("Could not open input file: \"%s\"\n", file_name);
+		exit(1);
+	}
+	*file_size = get_file_size(ifp);
+	/* initialize socket connection in unix domain */
+	if((sock = SOCKET(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("error opening socket");
+		exit(1);
+	}
+	hp = gethostbyname(argv[1]);
+	if(hp == 0) 
+	{
+		fprintf(stderr, "%s: unknown host\n", argv[1]);
+		exit(2);
+	}
+	/* construct name of socket to send to */
+	bcopy((void *)hp->h_addr, (void *)&to_send.sin_addr, hp->h_length);
+	to_send.sin_family = htons(AF_INET);
+	if((to_send.sin_port = htons(get_port(argv))) < 0)
+	{
+		exit(1);
+	}
+	//
+	/* establish connection with server */
+	//copy file size into buffer
+	*file_size = htonl(*file_size);
+	memcpy(packet.buff, file_size,4);
+	memcpy(file_size,packet.buff,4);
+
+	//copy file name into buffer
+	memcpy(packet.buff + sizeof(int), file_name ,20);
+	memcpy(file_name,packet.buff+4,20);
+	/* write buf to sock */	
+	packet.address = to_send;
+	printf("port:%d\n",ntohs(packet.address.sin_port));
+	if(SEND(sock,&packet,60,0) < 0) 
+	{
+		perror("error writing on stream socket");
+		exit(1);
+	}
+	printf("Size of tcp header: %d\n",sizeof(struct tcphdr));
+	printf("Client sends file size: %i and filename: %s\n", ntohl(*file_size), file_name);
+	int sent = 0;
+	int total_sent = 0;
+	while(total_sent < ntohl(*file_size)){
+		sent = fread(packet.buff,1,1024,ifp);
+		
+		total_sent += sent;
+		if(SEND(sock,&packet,sent+16+20,0)< 0) 
+		{
+			perror("error writing on stream socket");
+			exit(1);
+		}
+		sleep(1);
+	}
+	printf("total sent: %d\n",total_sent);
+	printf("Client done sending file\n");
+	free(file_size);
+	fclose(ifp);
+}
---
+++ finalProject/tcpdClient.c
@@ -0,0 +1,139 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<errno.h>
+#include<limits.h>
+#include <strings.h>
+#include <string.h>
+#include <linux/tcp.h>
+#include "CapitalFunctions.h"
+
+#define LOCAL_PORT 6650
+#define REMOTE_PORT 9980
+#define CRC16 0x8005
+// source:http://stackoverflow.com/questions/10564491/function-to-calculate-a-crc16-checksum
+uint16_t gen_crc16(const uint8_t *data, uint16_t size)
+{
+    uint16_t out = 0;
+    int bits_read = 0, bit_flag;
+
+    /* Sanity check: */
+    if(data == NULL)
+        return 0;
+
+    while(size > 0)
+    {
+        bit_flag = out >> 15;
+
+        /* Get next bit: */
+        out <<= 1;
+        out |= (*data >> bits_read) & 1; // item a) work from the least significant bits
+
+        /* Increment bit counter: */
+        bits_read++;
+        if(bits_read > 7)
+        {
+            bits_read = 0;
+            data++;
+            size--;
+        }
+
+        /* Cycle check: */
+        if(bit_flag)
+            out ^= CRC16;
+
+    }
+
+    // item b) "push out" the last 16 bits
+    int i;
+    for (i = 0; i < 16; ++i) {
+        bit_flag = out >> 15;
+        out <<= 1;
+        if(bit_flag)
+            out ^= CRC16;
+    }
+
+    // item c) reverse the bits
+    uint16_t crc = 0;
+    i = 0x8000;
+    int j = 0x0001;
+    for (; i != 0; i >>=1, j <<= 1) {
+        if (i & out) crc |= j;
+    }
+
+    return crc;
+}
+int main(int argc, char* argv[]){
+/*initialize socket connection in unix domain*/
+	int local_sock, remote_sock;		/* initial socket descriptor */
+	struct sockaddr_in local_sin_addr;		/* structure for socket name setup */
+	struct sockaddr_in remote_sin_addr;
+	struct sockaddr_in src_addr;
+	struct sockaddr_in troll;
+	struct sockaddr_in empty;
+	int src_addr_len;
+	int addr_len;
+	struct hostent *hp;
+	char buf[1060];		/* buffer for holding read data */
+	if((local_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("error opening datagram socket");
+		exit(1);
+	}
+	 /* create name with parameters and bind name to socket */
+    local_sin_addr.sin_family = AF_INET;
+    local_sin_addr.sin_port = htons(LOCAL_PORT);
+    local_sin_addr.sin_addr.s_addr = INADDR_ANY;
+    if(bind(local_sock, (struct sockaddr *)&local_sin_addr, sizeof(local_sin_addr)) < 0) {
+	perror("getting socket name");
+	exit(2);
+    }
+    addr_len=sizeof(struct sockaddr_in);
+    /* Find assigned port value and print it for client to use */
+    if(getsockname(local_sock, (struct sockaddr *)&local_sin_addr, &src_addr_len) < 0){
+	perror("getting sock name");
+	exit(3);
+    }
+    printf("tcpdClient local port: %d\n", ntohs(local_sin_addr.sin_port));
+	/* put all zeros in buffer (clear) */
+	bzero(buf,1060);
+	/////////////////////////////////////////////////////////////////////////////////
+	//remote port below here
+	////////////////////////////////////////////////////////////////////////////////
+	if((remote_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("error opening datagram socket");
+		exit(1);
+	}
+	 /* create name with parameters and bind name to socket */
+    remote_sin_addr.sin_family = AF_INET;
+    remote_sin_addr.sin_port = htons(REMOTE_PORT);
+    char beta[] = "beta";
+	hp = gethostbyname(beta);
+	bcopy((void *)hp->h_addr, (void *)&remote_sin_addr.sin_addr, hp->h_length);
+    printf("tcpdClient remote port: %d\n", ntohs(remote_sin_addr.sin_port));
+    struct Packet crc;
+    
+	/* put all zeros in buffer (clear) */
+	bzero(buf,1060);
+	int i = 0;
+	while(1){
+		ssize_t pie = recvfrom(local_sock, buf, 1060, 0, (struct sockaddr *)&src_addr , &src_addr_len);
+		//send to troll
+		
+		memcpy(&crc,buf,pie);
+		crc.TCPHeader.check = 0;
+		memcpy(buf,&crc,pie);
+		crc.TCPHeader.check= gen_crc16(buf+16,pie-16);
+		memcpy(buf,&crc,pie);
+		i++;
+		printf("Packet:     size: %d    CRC:   %d\n",i, pie,crc.TCPHeader.check);
+		printf("\n");
+		
+		sendto(remote_sock, buf, pie, 0, (struct sockaddr *)&remote_sin_addr, sizeof(remote_sin_addr));
+		printf("sent packet\n");
+	}
+}
